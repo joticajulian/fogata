80c05ea73e4befa9db9cc2df5a3350d41ab19c3d
Julian Gonzalez <joticajulian@gmail.com>
2022-11-12 23:56:09 +0000

    wip - update fees, unstake function

--- contracts/assembly/Fogata.ts
+++ contracts/assembly/Fogata.ts
@@ -20,6 +20,10 @@ export class Fogata {
 
   stakes: Storage.Map<Uint8Array, common.uint64>;
 
+  nodeOperatorFees: Storage.Obj<common.uint64>;
+
+  lastPoolVirtual: Storage.Obj<common.uint64>;
+
   constructor() {
     this.contractId = System.getContractId();
 
@@ -37,6 +41,67 @@ export class Fogata {
       common.uint64.encode,
       () => new common.uint64(0)
     );
+    this.nodeOperatorFees = new Storage.Obj(
+      this.contractId,
+      2,
+      common.uint64.decode,
+      common.uint64.encode,
+      () => new common.uint64(0)
+    );
+    this.lastPoolVirtual = new Storage.Obj(
+      this.contractId,
+      2,
+      common.uint64.decode,
+      common.uint64.encode,
+      () => new common.uint64(0)
+    );
+  }
+
+  /**
+   * Function to update the fees taken by the operator
+   * if any, based on the virtual balance of the pool.
+   *
+   * This function is done in a way that if it's called
+   * twice, the second call will not have effect because the
+   * fees will be already taken
+   */
+  updateNodeOperatorFee(
+    contractBalance: u64,
+    saveLastPoolVirtual: boolean
+  ): u64 {
+    const lastPoolVirtual = this.lastPoolVirtual.get()!;
+    const nodeOperatorFees = this.nodeOperatorFees.get()!;
+
+    // This contract contains in its balance the tokens of
+    // the users and the tokens of the node operator. The process
+    // to calculate the fees is as follows:
+
+    // Take the total balance and remove the fees already earned
+    // by the operator to get the new virtual balance
+    const poolVirtualAndFee = contractBalance - nodeOperatorFees.value;
+
+    // check how much this virtual balance has increased
+    const deltaPoolVirtual = poolVirtualAndFee - lastPoolVirtual.value;
+    if (deltaPoolVirtual < 0) error;
+
+    // calculate new fees earned by the node operator
+    const newFee = deltaPoolVirtual * fee_percentage;
+
+    // calculate the new virtual balance of the pool
+    const poolVirtual = poolVirtualAndFee - newFee;
+
+    // update values
+    nodeOperatorFees.value += newFee;
+    this.nodeOperatorFees.put(nodeOperatorFees);
+
+    // option indicating if lastPoolVirtual should be updated
+    // now or if it will be done later (to reduce calls to the
+    // system)
+    if (saveLastPoolVirtual) {
+      lastPoolVirtual.value = poolVirtual;
+      this.lastPoolVirtual.put(lastPoolVirtual);
+    }
+    return poolVirtual;
   }
 
   /**
@@ -53,9 +118,10 @@ export class Fogata {
     const vhpContract = new Token(System.getContractAddress("vhp"));
 
     // get the virtual balance of pool before making the transfer
-    const poolVirtual =
+    const contractBalance =
       koinContract.balanceOf(this.contractId) +
       vhpContract.balanceOf(this.contractId);
+    const poolVirtualOld = this.updateNodeOperatorFee(contractBalance, false);
 
     // burn KOINs in the same account to get VHP
     new PoB().burn(
@@ -63,11 +129,11 @@ export class Fogata {
     );
 
     // transfer all VHP to the pool
-    const userVirtual = args.koin_amount + args.vhp_amount;
+    const deltaUserVirtual = args.koin_amount + args.vhp_amount;
     const transferStatus = vhpContract.transfer(
       args.account!,
       this.contractId,
-      userVirtual
+      deltaUserVirtual
     );
     System.require(
       transferStatus == true,
@@ -75,11 +141,11 @@ export class Fogata {
     );
 
     // calculate stake of the user
-    let userStake: u64;
+    let deltaUserStake: u64;
     const poolStake = this.poolStake.get()!;
     if (poolStake.value == 0) {
       // this is the first stake in the pool
-      userStake = userVirtual;
+      deltaUserStake = deltaUserVirtual;
     } else {
       // the user is adding stake and virtual balance to the pool. The
       // following relation must be preserved to not affect previous users:
@@ -87,32 +153,105 @@ export class Fogata {
       // poolStake_new / poolVirtual_new = poolStake_old / poolVirtual_old
       //
       // where:
-      // poolStake_new = poolStake_old + userStake
-      // poolVirtual_new = poolVirtual_old + userVirtual
+      // poolStake_new = poolStake_old + delta_userStake
+      // poolVirtual_new = poolVirtual_old + delta_userVirtual
       //
       // after some math the new stake for the user is calculated as:
-      // userStake = userVirtual * poolStake_old / poolVirtual_old
-      userStake = (userVirtual * poolStake.value) / poolVirtual;
+      // delta_userStake = delta_userVirtual * poolStake_old / poolVirtual_old
+      deltaUserStake = (deltaUserVirtual * poolStake.value) / poolVirtualOld;
       // todo: use bigint
     }
 
     // add new stake to the user
-    const userStakeInstace = this.stakes.get(args.account!)!;
-    userStakeInstace.value += userStake;
-    this.stakes.put(args.account!, userStakeInstace);
+    const userStake = this.stakes.get(args.account!)!;
+    userStake.value += deltaUserStake;
+    this.stakes.put(args.account!, userStake);
 
     // update the total pool stake
-    poolStake.value += userStake;
+    poolStake.value += deltaUserStake;
     this.poolStake.put(poolStake);
+
+    // update last pool virtual
+    const poolVirtualNew = poolVirtualOld + deltaUserVirtual;
+    this.lastPoolVirtual.put(new common.uint64(poolVirtualNew));
 
     System.event(
       "fogata.stake",
       Protobuf.encode(
-        new fogata.stake_event(args.account!, userVirtual, userStake),
+        new fogata.stake_event(args.account!, deltaUserVirtual, deltaUserStake),
         fogata.stake_event.encode
       ),
       [args.account!]
     );
+    return new common.boole(true);
+  }
+
+  /**
+   * Withdraw koin or vhp from the pool
+   * @external
+   */
+  unstake(args: fogata.stake_args): common.boole {
+    // contract definitions
+    const koinContract = new Token(System.getContractAddress("koin"));
+    const vhpContract = new Token(System.getContractAddress("vhp"));
+
+    // get the virtual balance of pool before making the transfer
+    const contractBalance =
+      koinContract.balanceOf(this.contractId) +
+      vhpContract.balanceOf(this.contractId);
+    const poolVirtualOld = this.updateNodeOperatorFee(contractBalance, false);
+
+    // get current stake of the user
+    const userStake = this.stakes.get(args.account!)!;
+
+    // calculate virtual amount of the user
+    //
+    // note: same maths applied as in the stake function. Check there
+    // for more details
+    const poolStake = this.poolStake.get()!;
+    const userVirtual = (userStake.value * poolVirtualOld) / poolStake.value;
+    // todo: use bigint
+
+    const deltaUserVirtual = args.koin_amount + args.vhp_amount;
+    System.require(
+      userVirtual >= deltaUserVirtual,
+      "insufficient virtual balance"
+    );
+
+    const deltaUserStake =
+      (deltaUserVirtual * poolStake.value) / poolVirtualOld;
+
+    // todo: check limits KOIN
+
+    if (args.koin_amount > 0) {
+      const transferStatus1 = koinContract.transfer(
+        this.contractId,
+        args.account!,
+        args.koin_amount
+      );
+      System.require(transferStatus1 == true, "transfer of koins rejected");
+    }
+    if (args.vhp_amount > 0) {
+      const transferStatus2 = vhpContract.transfer(
+        this.contractId,
+        args.account!,
+        args.vhp_amount
+      );
+      System.require(transferStatus2 == true, "transfer of koins rejected");
+    }
+
+    // remove stake from the user
+    userStake.value -= deltaUserStake;
+    this.stakes.put(args.account!, userStake);
+
+    // update the total pool stake
+    poolStake.value -= deltaUserStake;
+    this.poolStake.put(poolStake);
+
+    // update last pool virtual
+    const poolVirtualNew = poolVirtualOld - deltaUserVirtual;
+    this.lastPoolVirtual.put(new common.uint64(poolVirtualNew));
+
     return new common.boole(true);
   }
 }
